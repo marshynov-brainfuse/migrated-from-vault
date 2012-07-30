bfa91793ea6add2386cfc901dc0139aa8ceaca65
Michael Arshynov <marshynov@brainfuse.com>
2012-07-30 15:55:16 -0400

    ﻿ [git-vault-id] TestImportRepo$/@4/158117

---
+++ folder1/quartz/java/src/com/eggtheory/brainfuse/server/quizfeedback/finisher/QuizFeedbackFinisherJob.java
@@ -0,0 +1,21 @@
+package com.eggtheory.brainfuse.server.quizfeedback.finisher;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+//import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class QuizFeedbackFinisherJob {
+//extends QuartzJobBean {
+//
+//	private QuizFeedbackFinisherTask quizFeedbackFinisherTask;
+//
+//	public void setQuizFeedbackFinisherTask(QuizFeedbackFinisherTask quizFeedbackFinisherTask) {
+//		this.quizFeedbackFinisherTask = quizFeedbackFinisherTask;
+//	}
+//	
+//	@Override
+//	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+//		quizFeedbackFinisherTask.markExpiredQuizFeedbacksAsDone();
+//	}
+
+
+}
