43dee2b4803aa73e1b34316c6336f0bbc8f4fe8b
Michael Arshynov <marshynov@brainfuse.com>
2012-08-02 13:04:55 -0400

    rm28

--- folder1/quartz/java/src/com/eggtheory/brainfuse/server/quizfeedback/finisher/QuizFeedbackFinisherTask.java
+++
@@ -1,9 +0,0 @@
-package com.eggtheory.brainfuse.server.quizfeedback.finisher;
-
-import java.util.Date;
-
-public class QuizFeedbackFinisherTask {
-	public void markExpiredQuizFeedbacksAsDone() {
-		System.out.println("QuizFeedbackFinisherTask: markExpiredQuizFeedbacksAsDone: "+new Date());
-	}
-}
